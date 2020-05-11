a361a74019704c14d2efbcc513e2ecd54f819e3e
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-11 03:29:35 +0100

    new

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,9 @@ return(
   <div className="Movie">
      <h2>{movie.Title}</h2>
      <img 
-       
+       width="200"
+       alt={`The movie tilted: ${movie.Title} `}
+       src={poster}
      />
   </div>
 )
