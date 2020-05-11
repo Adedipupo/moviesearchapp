2318f8f1c6d1f3cb4ce8586c664942bf18fa995a
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-11 03:21:12 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div>dipo</div>;
+  return (
+    <>
+      <Header text="Movie Search App" />
+    </>
+  );
 }
 
 export default App;
