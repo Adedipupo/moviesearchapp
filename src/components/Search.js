a9a7c285b36bb1ea1b1c1853854065ba1b297e66
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-11 03:37:27 +0100

    new

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+const Search = (props) => {
+    const [searchValue, setSearchValue] = useState("");
+
 
 const handleSearchInputChanges = (e) => {
     setSearchValue(e.target.value);
