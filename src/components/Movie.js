809a1649b16546da7e3623c237045fb348812714
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-11 03:32:28 +0100

    new

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -11,13 +11,16 @@ const Movie = ({movie}) => {
 return(
   <div className="Movie">
      <h2>{movie.Title}</h2>
+   <div>  
      <img 
        width="200"
        alt={`The movie tilted: ${movie.Title} `}
        src={poster}
      />
   </div>
-)
-}
+  <p>{movie.Year}</p>
+  </div>
+);
+};
 
 export default Movie;
