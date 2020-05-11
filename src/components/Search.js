aed7105185981dfbfbd1a37cc65c0edf1708cd66
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-11 03:36:09 +0100

    new

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 const Search = () => {
 return(
-  <div>
-     
-  </div>
-)
+    <form className="search">
+        <input
+            value={searchValue}
+            onChange={handleSearchInputChanges}
+            type="text"
+        />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+    </form>
+);
 }
 
 export default Search;
