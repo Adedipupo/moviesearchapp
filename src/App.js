1a1d38c08731a221efb92be4d6202a2686c134a5
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-11 03:53:48 +0100

    new

--- src/App.js
+++ src/App.js
@@ -12,6 +12,31 @@ const initialState = {
   errorMessage: null
 };
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
+  }
+};
+
 
 const App = () => {
   const [loading, setLoading] = useState(true);
